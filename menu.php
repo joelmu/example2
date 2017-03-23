0e331d076cf2ce7647da0464f7f541718dacffc7
joelmu <joel.muittari@gmail.com>
2017-03-23 09:10:04 +0200

    editing

--- menu.php
+++ menu.php
@@ -16,6 +16,7 @@
         <li><a href="customer.php">Customers</a></li>
         <li><a href="add_customer.php">AddCustomers</a></li>
         <li><a href="cats.php">Cats</a></li>
+        <li><a href="edit_customers.php">Edit customers</a></li>
       </ul>
     </div>
     <div id="content">
